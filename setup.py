7770a69ced381b59b16b20b447886f7d3f241896
Jannis Leidel <jannis@leidel.info>
2013-09-09 11:03:00 +0200

    Drop d2to1.

--- setup.py
+++ setup.py
@@ -1,4 +1,47 @@
-#!/usr/bin/env python
+from __future__ import print_function
+import os
+import re
+import codecs
 from setuptools import setup
 
-setup(setup_requires=['d2to1'], d2to1=True)
+
+def read(*parts):
+    filename = os.path.join(os.path.dirname(__file__), *parts)
+    with codecs.open(filename, encoding='utf-8') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(
+    name="django-configurations",
+    version=find_version("configurations", "__init__.py"),
+    url='http://django-configurations.readthedocs.org/',
+    license='BSD',
+    description="A helper for organizing Django settings.",
+    long_description=read('README.rst'),
+    author='Jannis Leidel',
+    author_email='jannis@leidel.info',
+    packages=['configurations'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Topic :: Utilities',
+    ],
+    zip_safe=False,
+)
