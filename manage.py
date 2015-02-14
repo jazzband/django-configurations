313f30305ca3ad1e94a71dbca07536353d3452fc
Benjamin ABEL <bbig26@gmail.com>
2015-02-14 16:10:41 +0100

    Override env variables for tests

--- manage.py
+++ manage.py
@@ -3,8 +3,8 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tests.settings.main')
-    os.environ.setdefault('DJANGO_CONFIGURATION', 'Test')
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings.main'
+    os.environ['DJANGO_CONFIGURATION'] = 'Test'
 
     from configurations.management import execute_from_command_line
 
