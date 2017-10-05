d364802a8aa8562371769f5445efb72a83786576
Daniel Hahler <github@thequod.de>
2017-10-05 02:42:34 +0200

    Fix Django 2.0: use new url syntax (#182)

--- tests/urls.py
+++ tests/urls.py
@@ -1,9 +1,6 @@
-from django.conf.urls import include, url
-
-# Uncomment the next two lines to enable the admin:
+from django.conf.urls import url
 from django.contrib import admin
-admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
 ]
