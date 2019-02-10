6314038a771941761d4b687d7cb14a79005e9660
Christoph Krybus <ckrybus@googlemail.com>
2019-02-10 16:02:46 +0100

    Update django trove classifiers

--- setup.py
+++ setup.py
@@ -46,6 +46,11 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Framework :: Django',
+        'Framework :: Django :: 1.11',
+        'Framework :: Django :: 2.0',
+        'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
+        'Framework :: Django :: 3.0',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
