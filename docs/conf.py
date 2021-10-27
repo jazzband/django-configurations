e0c6cedab23b80288fd9aeac5a3489732f5ad215
Brian Helba <brian.helba@kitware.com>
2021-10-26 22:09:50 -0400

    Update the copyright date range

--- docs/conf.py
+++ docs/conf.py
@@ -42,7 +42,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'django-configurations'
-copyright = '2012-2014, Jannis Leidel and other contributors'
+copyright = '2012-2021, Jannis Leidel and other contributors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
